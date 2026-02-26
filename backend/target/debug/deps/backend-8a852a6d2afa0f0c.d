bab672cf3a32e69ec9725c78610e0f763fbc354a
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-02-26 12:28:54 +0530

    cretae the frontebnd and backend useing rust and next

---
+++ backend/target/debug/deps/backend-8a852a6d2afa0f0c.d
@@ -0,0 +1,5 @@
+/Users/manojseetaramgowda/projects/gpt/backend/target/debug/deps/backend-8a852a6d2afa0f0c.d: src/main.rs
+
+/Users/manojseetaramgowda/projects/gpt/backend/target/debug/deps/libbackend-8a852a6d2afa0f0c.rmeta: src/main.rs
+
+src/main.rs:
---
+++ backend/target/debug/deps/backend-8c1ae49e7a7fdd7f.d
@@ -0,0 +1,5 @@
+/Users/manojseetaramgowda/projects/gpt/backend/target/debug/deps/backend-8c1ae49e7a7fdd7f.d: src/main.rs
+
+/Users/manojseetaramgowda/projects/gpt/backend/target/debug/deps/libbackend-8c1ae49e7a7fdd7f.rmeta: src/main.rs
+
+src/main.rs:
