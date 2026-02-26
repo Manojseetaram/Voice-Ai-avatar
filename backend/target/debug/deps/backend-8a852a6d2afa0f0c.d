d8ccef2d2357ec0db38476594acec9836d65e545
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-02-26 12:33:55 +0530

    Remove target folder from tracking

--- backend/target/debug/deps/backend-8a852a6d2afa0f0c.d
+++
@@ -1,5 +0,0 @@
-/Users/manojseetaramgowda/projects/gpt/backend/target/debug/deps/backend-8a852a6d2afa0f0c.d: src/main.rs
-
-/Users/manojseetaramgowda/projects/gpt/backend/target/debug/deps/libbackend-8a852a6d2afa0f0c.rmeta: src/main.rs
-
-src/main.rs:
--- backend/target/debug/deps/backend-8c1ae49e7a7fdd7f.d
+++
@@ -1,5 +0,0 @@
-/Users/manojseetaramgowda/projects/gpt/backend/target/debug/deps/backend-8c1ae49e7a7fdd7f.d: src/main.rs
-
-/Users/manojseetaramgowda/projects/gpt/backend/target/debug/deps/libbackend-8c1ae49e7a7fdd7f.rmeta: src/main.rs
-
-src/main.rs:
